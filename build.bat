678adc10b9933581107951169aec24a6fcc69468
Nathan du Buy <nathandubuy4@gmail.com>
2026-08-02 19:15:57 +0200

    added --msvc flag to make sure its compiled by msvc

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 Title compiling chr.py
 echo Compiling chr.py with Nuitka...
 
-python -m nuitka --standalone --output-dir=dist --remove-output --windows-console-mode=disable --enable-plugin=tk-inter --include-package=requests --windows-icon-from-ico=assets/chr.ico src/chr.py
+python -m nuitka --standalone --msvc=latest --output-dir=dist --remove-output --windows-console-mode=disable --enable-plugin=tk-inter --include-package=requests --windows-icon-from-ico=assets/chr.ico src/chr.py
